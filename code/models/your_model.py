1082dc9285e823ee33c2cf69b008b229b4de7404
tarena <562034100@qq.com>
2020-09-02 17:44:20 +0800

    initial commit

--- code/models/your_model.py
+++ code/models/your_model.py
@@ -9,9 +9,12 @@ import os, pickle
 from tqdm import tqdm
 import numpy as np
 from time import strftime, gmtime
+import pandas as pd
+
 
 def get_time_str():
     return strftime("%Y%m%d_%H%M%S", gmtime())
+
 
 def print_and_log(log_name, my_str):
     out = '{}|{}'.format(get_time_str(), my_str)
@@ -19,16 +22,19 @@ def print_and_log(log_name, my_str):
     with open(log_name, 'a') as f_log:
         print(out, file=f_log)
 
+
 def save_checkpoint(state, path):
-    filename = 'checkpoint_{.4f}.pth'.format(state['val_auc'])
+    filename = 'checkpoint_{:.4f}.pth'.format(state['val_auroc'])
     filename = os.path.join(path, filename)
     torch.save(state, filename)
+
 
 def reshape_input(arr):
     new_arr = []
     for i in range(arr.shape[0]):
         new_arr.append(arr[i].T)
     return np.array(new_arr)
+
 
 def get_model_savedir(dir):
     lst = os.listdir(dir)
@@ -39,37 +45,58 @@ def get_model_savedir(dir):
     new_lst.sort()
     return new_lst[-1]
 
+
+def aggreate_predict(all_pid, gt, prob):
+    # group by pid
+    final_pred = []
+    final_gt = []
+    pid_set = set(all_pid)
+    for pid in pid_set:
+        select_idx = (all_pid == pid)
+        tmp_pred = prob[select_idx]
+        tmp_gt = gt[select_idx]
+
+        final_pred.append(np.max(tmp_pred, axis=0))
+        final_gt.append(tmp_gt[0])
+
+    return np.array(final_gt), np.array(final_pred)
+
+
 class YourModel(ClassificationModel):
-    def __init__(self, name, n_classes,  sampling_frequency, outputfolder, input_shape):
+    def __init__(self, name, n_classes, sampling_frequency, outputfolder, input_shape):
         self.name = name
         self.n_classes = n_classes
         self.sampling_frequency = sampling_frequency
         self.outputfolder = outputfolder
-        self.input_shape = input_shape 
+        self.input_shape = input_shape
+        self.gpu_id = 6
+        self.model = Net1D(in_channels=12, base_filters=64, ratio=1, filter_list=[64, 160, 160, 400, 400, 1024, 1024],
+                      m_blocks_list=[2, 2, 2, 3, 3, 4, 4], kernel_size=16, stride=2, groups_width=16, verbose=False,
+                      use_bn=True, use_do=True, n_classes=self.n_classes)
+        self.batch_size = 1024
+        self.device = torch.device('cuda:{}'.format(self.gpu_id) if torch.cuda.is_available() else 'cpu')
 
-    def fit(self, X_train, y_train, X_val, y_val):
-        gpu_id = 1
+
+    def fit(self, X_train, y_train, X_val, y_val, pid_val):
 
         ### no need to change
-        batch_size = 128
         lr = 1e-3
         weight_decay = 1e-4
         early_stop_lr = 1e-5
-        Epochs = 10
-        eval_steps = 20
+        Epochs = 50
+        eval_steps = 5
 
         ### data
         trainset_x, trainset_y = X_train, y_train
         valset_x, valset_y = X_val, y_val
-
-        device = torch.device('cuda:{}'.format(gpu_id) if torch.cuda.is_available() else 'cpu')
+        valset_pid = pid_val
+        print(trainset_x.shape, trainset_y.shape)
+        print(valset_x.shape, valset_y.shape)
 
         ### model net1d
-        model = Net1D(in_channels=12,base_filters=64,ratio=1,filter_list=[64, 160, 160, 400, 400, 1024, 1024],
-            m_blocks_list=[2, 2, 2, 3, 3, 4, 4],kernel_size=16,stride=2,groups_width=16,verbose=False,
-            use_bn=False,use_do=False,n_classes=self.n_classes)
+        model = self.model
 
-        model.to(device)
+        model.to(self.device)
 
         criterion = nn.BCEWithLogitsLoss()
         optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
@@ -79,17 +106,18 @@ class YourModel(ClassificationModel):
         best_val_auroc = 0.
         step = 0
 
+        all_res = []
         for epoch in range(Epochs):
 
             ### train
-            for idx in tqdm(range(0, trainset_x.shape[0], batch_size), desc='Training'):
-
-
-                input_x, input_y = trainset_x[idx:idx+batch_size], trainset_y[idx:idx+batch_size]
+            for idx in tqdm(range(0, trainset_x.shape[0], self.batch_size), desc='Training'):
+                input_x, input_y = trainset_x[idx:idx + self.batch_size], trainset_y[idx:idx + self.batch_size]
+                # input_x = np.expand_dims(input_x.T, axis=0)
+                # input_y = np.expand_dims(input_y, axis=0)
                 input_x = reshape_input(input_x)
-                input_x = torch.from_numpy(input_x).to(device).float()
-                input_y = torch.from_numpy(input_y).to(device).float()
-
+                input_x = torch.from_numpy(input_x).to(self.device).float()
+                input_y = torch.from_numpy(input_y).to(self.device).float()
+                # print(input_x.shape)
                 outputs = model(input_x)
                 loss = criterion(outputs, input_y)
                 optimizer.zero_grad()
@@ -99,28 +127,39 @@ class YourModel(ClassificationModel):
                 step += 1
 
                 if step % eval_steps == 0:
+                    train_loss = loss.cpu().data.numpy()
                     # val
                     model.eval()
                     all_gt = []
                     all_pred_prob = []
+                    val_loss = []
                     with torch.no_grad():
-                        for idx in tqdm(range(valset_x.shape[0]), desc='Validating'):
-                            input_x, input_y = valset_x[idx], valset_y[idx]
+                        for idx in tqdm(range(0, valset_x.shape[0], self.batch_size), desc='Validating'):
+                            input_x, input_y = valset_x[idx:idx + self.batch_size], valset_y[idx:idx + self.batch_size]
                             input_x = reshape_input(input_x)
-                            input_x = torch.from_numpy(input_x).to(device).float()
-                            input_y = torch.from_numpy(input_y).to(device).float()
+                            # input_x = np.expand_dims(input_x.T, axis=0)
+                            # input_y = np.expand_dims(input_y, axis=0)
+                            input_x = torch.from_numpy(input_x).to(self.device).float()
+                            input_y = torch.from_numpy(input_y).to(self.device).float()
+                            # print(input_x.shape)
                             logits = model(input_x)
+                            v_loss = criterion(logits, input_y)
+                            val_loss.append(v_loss.cpu().data.numpy())
+
                             pred = torch.sigmoid(logits)
                             all_pred_prob.append(pred.cpu().data.numpy())
                             all_gt.append(input_y.cpu().data.numpy())
                     all_pred_prob = np.concatenate(all_pred_prob)
                     all_gt = np.concatenate(all_gt)
                     all_gt = np.array(all_gt)
-
+                    val_loss = np.array(val_loss).mean()
+                    # print(all_pred_prob.shape)
+                    all_gt, all_pred_prob = aggreate_predict(valset_pid, all_gt, all_pred_prob)
+                    # print(all_pred_prob.shape)
                     val_auroc = roc_auc_score(all_gt, all_pred_prob)
 
                     # save train_log
-                    log_str = 'Epoch {} step {}, val: {:.4f}'.format(epoch, step, val_auroc)
+                    log_str = 'Epoch {} step {}, val_auroc: {:.4f}'.format(epoch, step, val_auroc)
                     log_name = os.path.join(self.outputfolder, 'log.txt')
                     print_and_log(log_name, log_str)
 
@@ -133,45 +172,53 @@ class YourModel(ClassificationModel):
                         save_checkpoint({
                             'epoch': epoch,
                             'step': step,
-                            'state_dict': model.state_dict()
+                            'state_dict': model.state_dict(),
+                            'val_auroc': val_auroc
                         }, saved_dir)
 
                     scheduler.step(val_auroc)
                     ### early stop
                     current_lr = optimizer.param_groups[0]['lr']
-                    if current_lr < early_stop_lr:
-                        print("Early stop")
-                        exit()
+                    all_res.append([epoch, step, train_loss, val_loss, current_lr])
+                    df = pd.DataFrame(all_res, columns=['epoch', 'step', 'train_loss', 'val_loss', 'lr'])
+                    df.to_csv(os.path.join(self.outputfolder, 'res.csv'), index=False, float_format='%.5f')
+
+                    # if current_lr < early_stop_lr:
+                    # print("Early stop")
+                    # exit()
 
                     model.train()  # set back to train
 
-    def predict(self, X):
-        gpu_id = 1
-        batch_size = 128
-        device = torch.device('cuda:{}'.format(gpu_id) if torch.cuda.is_available() else 'cpu')
+    def predict(self, X, pid):
 
-        ### model net1d
-        model = Net1D(in_channels=12, base_filters=64, ratio=1, filter_list=[64, 160, 160, 400, 400, 1024, 1024],
-                      m_blocks_list=[2, 2, 2, 3, 3, 4, 4], kernel_size=16, stride=2, groups_width=16, verbose=False,
-                      use_bn=False, use_do=False, n_classes=self.n_classes)
 
         model_savedir = get_model_savedir(self.outputfolder)
         checkpoint = torch.load(self.outputfolder + model_savedir)
+        model = self.model
         model.load_state_dict(checkpoint['state_dict'])
-        model.to(device)
+        model.to(self.device)
 
         model.eval()
         all_pred_prob = []
         with torch.no_grad():
-            for idx in tqdm(range(0, X.shape[0], batch_size), desc='Predicting'):
-                input_x = X[idx:idx+batch_size]
+            for idx in tqdm(range(0, X.shape[0], self.batch_size), desc='Predicting'):
+                input_x = X[idx:idx + self.batch_size]
                 input_x = reshape_input(input_x)
-                input_x = torch.from_numpy(input_x).to(device).float()
+                input_x = torch.from_numpy(input_x).to(self.device).float()
                 logits = model(input_x)
                 pred = torch.sigmoid(logits)
                 all_pred_prob.append(pred.cpu().data.numpy())
         all_pred_prob = np.concatenate(all_pred_prob)
+        # print('pred on test de shape: ', all_pred_prob.shape)
+        pid_set = set(pid)
+        final_pred = []
+        for p in pid_set:
+            select_idx = (pid == p)
+            tmp_pred = all_pred_prob[select_idx]
+            final_pred.append(np.max(tmp_pred, axis=0))
+        # print(np.array(final_pred).shape)
+        return np.array(final_pred)
 
-        return all_pred_prob
+
 
 
